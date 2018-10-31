08e6c6d8a57a6517de2850a880a216da2cef18da
polosesy <44423738+polosesy@users.noreply.github.com>
2018-10-31 10:10:50 +0900

    hotfix branch change a code

--- Hello world/src/HelloWorld/Hello.java
+++ Hello world/src/HelloWorld/Hello.java
@@ -4,7 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("World's End Dancehall");
-		System.out.println("Earth's End Dancehall");
+		System.out.println("Hello Dance World");
 	}
-
 }
