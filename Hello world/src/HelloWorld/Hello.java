808e723765408d16217ff1922653a7f5a3387017
polosesy <44423738+polosesy@users.noreply.github.com>
2018-10-31 11:17:01 +0900

    merge hotfix

--- Hello world/src/HelloWorld/Hello.java
+++ Hello world/src/HelloWorld/Hello.java
@@ -4,9 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("World's End Dancehall");
-<<<<<<< HEAD
-=======
 		System.out.println("Hello Dance World");
->>>>>>> refs/heads/hofix
 	}
 }
