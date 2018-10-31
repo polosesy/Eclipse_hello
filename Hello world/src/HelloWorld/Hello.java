318d59e0d26464c3ea1ee64177ed8d3bf02a29ce
polosesy <44423738+polosesy@users.noreply.github.com>
2018-10-31 14:56:59 +0900

    Merage branch 'hotfix' by yeong il

--- Hello world/src/HelloWorld/Hello.java
+++ Hello world/src/HelloWorld/Hello.java
@@ -5,10 +5,7 @@ public class Hello {
 		System.out.println("Hello World");
 		System.out.println("World's End Dancehall");
 		System.out.println("Hello Dance World");
-<<<<<<< HEAD
 		System.out.println("Earth's End Dancehall");
-=======
 		System.out.println("Ho Woo~~");
->>>>>>> refs/heads/hofix
 	}
 }
